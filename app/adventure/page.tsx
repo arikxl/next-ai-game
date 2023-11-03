afc79892efa250f3ae0acc1e9921864abc59ccf5
Arik Alexandrov <arikxl@gmail.com>
2023-11-03 21:30:37 +0200

    init again

---
+++ app/adventure/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const adventurePage = () => {
+  return (
+    <div>
+      <h1>
+        ssss
+      </h1>
+
+      
+    </div>
+  )
+}
+
+export default adventurePage
